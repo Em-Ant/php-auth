de46b140ed83faa7eab34ee32939123d1b9d6408
emant <joshua999x@gmail.com>
2022-07-22 23:06:02 +0200

    fix: add viewport tag

--- src/views/template.php
+++ src/views/template.php
@@ -8,6 +8,7 @@ $title = $title ?: 'Auth';
 
 <head>
   <title><?= $title ?></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="<?= $sub_path ?>/public/favicon.ico" type="image/x-icon" />
   <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" type="text/css">
   <link href="<?= $sub_path ?>/public/style.css" rel="stylesheet" type="text/css">
